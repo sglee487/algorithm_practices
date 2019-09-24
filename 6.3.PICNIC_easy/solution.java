7df7544f3407d155cf2def636433f014fc80e67a
sglee487 <sglee487@gmail.com>
2019-09-25 02:03:10 +0900

    문제 input output 건드리는 중

--- 6.3.PICNIC_easy/solution.java
+++ 6.3.PICNIC_easy/solution.java
@@ -18,26 +18,8 @@ import java.util.Scanner;
 */
 public class solution {
     public static void main(String[] args){
-        Scanner keyboard = new Scanner(System.in);
-
         // 먼저 답에서 봤듯이 boolean 을 가지는 2차원 배열.
         boolean[][] areFriends = new boolean[10][10];
-        int TestCase = keyboard.nextInt();
-        int HowManyPeople, HowManyTuple;
-        List PeopleArray = new ArrayList<Integer>();
-        for (int i=0;i<TestCase;i++){
-            HowManyPeople = keyboard.nextInt();
-            HowManyTuple = keyboard.nextInt();
-            for (int j=0;j<HowManyTuple;j++){
-                PeopleArray.add(keyboard.nextInt());
-                PeopleArray.add(keyboard.nextInt());
-            }
-            // 여기서부터 모든 정보가 들어있으니 계산
-
-            System.out.println(TestCase);
-            System.out.println(PeopleArray.get(0));
-            System.out.println(PeopleArray.get(1));
-        }
         System.out.println(areFriends[0][0]);
         areFriends[0][0] = true;
         System.out.println(areFriends[0][0]);
