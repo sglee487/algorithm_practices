e40a53b0ab4bcdac99659a0b4c6c344131a70d67
sglee487 <sglee487@gmail.com>
2019-09-26 01:35:11 +0900

    문제 input output 건드리는 중

--- 6.3.PICNIC_easy/read_practice.java
+++ 6.3.PICNIC_easy/read_practice.java
@@ -10,7 +10,7 @@ import static java.util.stream.Collectors.toList;
 public class read_practice {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+//        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
 
         List<Integer> a = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
                 .map(Integer::parseInt)
@@ -27,7 +27,7 @@ public class read_practice {
 //                        + "\n"
 //        );
 
-        bufferedReader.close();
-        bufferedWriter.close();
+//        bufferedReader.close();
+//        bufferedWriter.close();
     }
 }
