79736db42f89e6912b1fe8cbcd6f0ca194b50439
sglee487 <sglee487@gmail.com>
2019-09-26 02:34:02 +0900

    6.5 문제. 입력 받고 출력 하는것까지 완료.

--- 6.5.BOARDCOVER_easy/practice.java
+++ 6.5.BOARDCOVER_easy/practice.java
@@ -30,9 +30,53 @@
  */
 
 
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.util.Scanner;
+
 public class practice {
 
-    public static void main(String[] args) {
+    static int calculateBoard(String[][] board){
+
+        return 0;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int q = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int qItr = 0; qItr < q; qItr++) {
+            int r = scanner.nextInt();
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+            int c = scanner.nextInt();
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            String[][] container = new String[r][c];
+
+            for (int i = 0; i < r; i++) {
+                String[] containerRowItems = scanner.nextLine().split("");
+                scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+                for (int j = 0; j < c; j++) {
+                    String containerItem = (containerRowItems[j]);
+                    container[i][j] = containerItem;
+                }
+            }
+
+            int result = calculateBoard(container);
+
+            bufferedWriter.write(String.valueOf(result));
+            bufferedWriter.newLine();
+        }
+
+        bufferedWriter.close();
+
+        scanner.close();
 
     }
 }
