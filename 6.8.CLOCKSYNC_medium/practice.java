aac479615fd6c2cbf2f246731117b0a8d7775cd8
sglee487 <sglee487@gmail.com>
2019-09-27 18:02:15 +0900

    6.8. 문제 CLOCKSYNC 매개변수 받게 함.

---
+++ 6.8.CLOCKSYNC_medium/practice.java
@@ -0,0 +1,56 @@
+/*
+스위치 번호      연결된 시계들     스위치 번호        연결된 시계들
+    0             0,1,2               5                0,2,14,15
+    1             3,7,9,11            6                3,14,15
+    2             4,10,14,15          7                4,5,7,14,15
+    3             0,4,5,6,7           8                1,2,3,4,5
+    4             6,7,8,10,12         9                3,4,5,9,13
+ */
+/*
+예제 입력
+2
+12 6 6 6 6 6 12 12 12 12 12 12 12 12 12 12
+12 9 3 12 6 6 9 3 12 9 12 9 12 12 6 6
+
+
+예제 출력
+2
+9
+ */
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.util.Scanner;
+
+public class practice {
+    private static final Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+//        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+//        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter());
+        BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int q = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+
+        int[] clock = new int[16];
+
+        for (int i = 0; i < q; i++) {
+            String[] containerRowItems = scanner.nextLine().split(" ");
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            for (int j = 0; j < containerRowItems.length; j++) {
+                int containerItem = Integer.parseInt(containerRowItems[j]);
+                clock[j] = containerItem;
+            }
+            bufferedWriter.write("str");
+            bufferedWriter.newLine();
+        }
+
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
